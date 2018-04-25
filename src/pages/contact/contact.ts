a79109c53ec2ef485bd2bf5e9a31959bcac96505
Fr4nck <lalevelteam@gmail.com>
2018-04-25 22:00:54 +0200

    Ajout Login + Ajout Slide Home

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-contact',
@@ -11,3 +11,4 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {
   }
 }
+
