89cb2b69af92f7b5cbc4d4b10bdc257c0e521f42
benarbia <benarbia@debian>
2018-05-01 11:14:25 +0200

    my test working

---
+++ Test/ionic3-angular5-rest/cp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp /home/benarbia/partage_linux/rest.ts ~/Test/ionic3-angular5-rest/src/providers/rest
+cp /home/benarbia/partage_linux/home.ts src/pages/home/ 
+cp /home/benarbia/partage_linux/home.html src/pages/home/
