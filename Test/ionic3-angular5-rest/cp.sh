35d1d192a48a2e0a04a89f98347e5d361f8ee1b8
WadiBenarbia <benarb_w@etna-alternance.net>
2018-05-03 15:59:34 +0200

    rm test

--- Test/ionic3-angular5-rest/cp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp /home/benarbia/partage_linux/rest.ts ~/Test/ionic3-angular5-rest/src/providers/rest
-cp /home/benarbia/partage_linux/home.ts src/pages/home/ 
-cp /home/benarbia/partage_linux/home.html src/pages/home/
