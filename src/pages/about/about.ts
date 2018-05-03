b1929000ec4c028f85d830c3ce513554cc14c460
root <root@DESKTOP-R6H435T.localdomain>
2018-05-03 12:26:51 +0200

    ajout du design

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: "page-about",
+  templateUrl: "about.html"
 })
 export class AboutPage {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  slider = [
+    {
+      title: "Slide1",
+      description: "c'est la premiere slide",
+      image: "../../assets/imgs/Healthkit_slide1.png"
+    },
+    {
+      title: "Slide2",
+      description: "c'est la deuxieme slide",
+      image: "../../assets/imgs/Healthkit_slide2.png"
+    },
+    {
+      title: "Slide3",
+      description: "c'est la troisieme slide",
+      image: "../../assets/imgs/Healthkit_slide3.jpg"
+    }
+  ];
 }
